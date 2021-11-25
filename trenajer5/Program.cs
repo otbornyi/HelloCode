9a2e96c16178be8604f9baf480174c92c824b532
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-25 19:43:09 +0300

    add trenajer5

---
+++ trenajer5/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = new Random().Next(1, 1000);
+Console.Write("A = ");
+Console.WriteLine(A);
+int B = new Random().Next(1, 1000);
+Console.Write("B = ");
+Console.WriteLine(B);
+int C = new Random().Next(1, 1000);
+Console.Write("C = ");
+Console.WriteLine(C);
+int max = A;
+if (B > max) max = B;
+
+if (C > max) max = C;
+Console.Write("max = ");
+Console.WriteLine(max);
