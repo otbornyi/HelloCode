612521d7047164a44e0e5a7a86a4efaed95b88b9
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-26 20:56:35 +0300

    add trenajer7

---
+++ trenajer7/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] N = new int[] {-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int index = 0;
+while (index < 18)
+{
+    Console.Write(N[index]);
+    Console.Write(" ");
+    index++;
+}
