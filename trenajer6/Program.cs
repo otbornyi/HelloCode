095bae45662bae15cd00bb76043f15d409e0db17
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-26 19:29:19 +0300

    add trenajer6

---
+++ trenajer6/Program.cs
@@ -0,0 +1,39 @@
+﻿int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+int c = new Random().Next(1, 100);
+if(a % 2 == 0)
+{
+    Console.Write("a = ");
+    Console.Write(a);
+    Console.WriteLine(" - число а четное ");
+}
+else
+{
+    Console.Write("a = ");
+    Console.Write(a);
+    Console.WriteLine(" - число а нечетное ");
+}
+if(b % 2 == 0)
+{
+    Console.Write("b = ");
+    Console.Write(b);
+    Console.WriteLine(" - число а четное ");
+}
+else
+{
+    Console.Write("b = ");
+    Console.Write(b);
+    Console.WriteLine(" - число а нечетное ");
+}
+if(c % 2 == 0)
+{
+    Console.Write("c = ");
+    Console.Write(c);
+    Console.WriteLine(" - число а четное ");
+}
+else
+{
+    Console.Write("c = ");
+    Console.Write(c);
+    Console.WriteLine(" - число а нечетное ");
+}
