7ea87c67b39ded65bc0a1418a88ddeae40f60691
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 21:00:40 +0300

    add trenajer10

---
+++ trenajer10/Program.cs
@@ -0,0 +1,9 @@
+﻿//int A = 764;
+//int B = A % 10;
+//int C = (A - (A % 100)) / 10 + B;
+//Console.WriteLine(C);
+int A = new Random().Next(100, 1000);
+Console.WriteLine(A);
+int B = A % 10;
+int C = (A - (A % 100)) / 10 + B;
+Console.WriteLine(C);
