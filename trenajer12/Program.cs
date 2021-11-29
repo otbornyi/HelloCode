d22f4bc18d788f9529141fb2b04f9bc31c8c28e9
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 21:49:30 +0300

    add trenajer12

---
+++ trenajer12/Program.cs
@@ -0,0 +1,20 @@
+﻿int A = new Random().Next(1, 100);
+Console.WriteLine(A);
+if(A > 999)
+{
+    int B = (A % 100);
+    int D = (B - (A % 10)) / 10;
+    Console.WriteLine(D);
+}
+else
+{
+    if((A / 10) > 9)
+    {
+    int C = A % 10;
+    Console.WriteLine(C);
+    }
+       else
+       {
+           Console.WriteLine("Третьей цифры в числе нету");
+       }
+}       
