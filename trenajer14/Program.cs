483e6dbe3adf0d79ab5cd30ccdbbc986dfb1afe0
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 22:45:17 +0300

    add trenajer14

---
+++ trenajer14/Program.cs
@@ -0,0 +1,37 @@
+﻿int Day = new Random().Next(1, 8);
+if(Day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(Day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(Day == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(Day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(Day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(Day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(Day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if(Day > 5)
+{
+    Console.WriteLine("Ура, сегодня отдыхаем");
+}
+else
+{
+    Console.WriteLine("Увы, сегодня работаем");
+}
