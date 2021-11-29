a800ee87c13b66f13a6f2ce69b5d5d3f8d63113e
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 20:48:34 +0300

    add treanjer9

--- trenajer9/Program.cs
+++ trenajer9/Program.cs
@@ -1,6 +1,16 @@
-﻿int A = new Random().Next(100, 1000);
-int B = A % 100;
-double x = floor(B)/10;
-Console.WriteLine(A);
-Console.WriteLine(B);
-Console.WriteLine(x);
+﻿int a = 46;
+string str = a.ToString();
+int[] b = new int[str.Length];
+for( int i=0; i<str.Length; i++)
+{
+    b[i] = int.Parse(str[i].ToString());
+}
+int n = b[0];
+if(b[1] > b[0])
+{
+    Console.WriteLine(b[1]);
+}
+else
+{
+    Console.WriteLine(b[0]);
+}
