20521b842ce97b03f919d20546d9107eb581660e
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-30 22:04:10 +0300

    add tranajer18

---
+++ trenajer18/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = 12;
+int B = 25;
+int C = 100 - (12 + 25);
+Console.WriteLine(C);
+int D = ((100 - 25 - 12));
+Console.WriteLine(D);
+if(C == D)
+{
+    Console.WriteLine("Утверждение ¬(12 ⋁ 25) = ¬12 ⋀ ¬25 - ВЕРНОЕ! ");
+}
+else
+{
+    Console.WriteLine("Утверждение ¬(12 ⋁ 25) = ¬12 ⋀ ¬25 - НЕВЕРНОЕ! ");
+}
