27082c8bae125ea36871c1b0b3c3b7e20bdc9905
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-30 19:15:37 +0300

    add trenajer15

---
+++ trenajer15/Program.cs
@@ -0,0 +1,29 @@
+﻿// // // Дан текст. В тексте нужно все пробелы заменить черточками, 
+// // // маленькие буквы “к” заменить большими “К”,
+// // // a большие “С” заменить маленькими “с”.
+// // // Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 1; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+return result;
+}    
+Console.WriteLine(text);
+Console.WriteLine();
+
+string newText = Replace(text, ' ', '!');
+Console.WriteLine(newText);    
+    
+           
