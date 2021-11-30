b6fe933ca278b713f41699d37bdc01b606bd0e43
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-30 20:45:48 +0300

    add treanjer17

---
+++ trenajer17/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = new Random().Next(1, 10);
+int B = new Random().Next(1, 100);
+Console.WriteLine(A);
+Console.WriteLine(B);
+if(A * A == B)
+{
+    Console.WriteLine("Ураааа нашли квадрат!!!");
+}
+else
+{
+    Console.WriteLine("Квадрат не найден");
+}
