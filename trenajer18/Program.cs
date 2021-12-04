1dadc2e3e38642b02ab9f759b8ac212642a4d8ac
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-04 22:33:24 +0300

    add trenajer 18

--- trenajer18/Program.cs
+++ trenajer18/Program.cs
@@ -1,14 +1,58 @@
-﻿int A = 12;
-int B = 25;
-int C = 100 - (12 + 25);
-Console.WriteLine(C);
-int D = ((100 - 25 - 12));
-Console.WriteLine(D);
-if(C == D)
+﻿Console.WriteLine("Условие : ");
+Console.WriteLine("Проверить истинность выражения !(X || Y) == (!X && !Y) ");
+bool X, Y;
+bool result = true;
+bool TrueFalse(bool X, bool Y)
 {
-    Console.WriteLine("Утверждение ¬(12 ⋁ 25) = ¬12 ⋀ ¬25 - ВЕРНОЕ! ");
+    return !(X || Y) == (!X && !Y);
 }
-else
+
+X = true;
+Y = true;
+bool A1 = TrueFalse(X, Y); Console.Write("Пара А1 - "); Console.WriteLine(A1);
+if(result == A1)
+
+X = true;
+Y = false;
+bool A2 = TrueFalse(X, Y); Console.Write("Пара А2 - "); Console.WriteLine(A2);
+if(result == A2)
+
+X = false;
+Y = true;
+bool A3 = TrueFalse(X, Y); Console.Write("Пара А3 - "); Console.WriteLine(A3);
+if(result == A3)
+
+X = false;
+Y = false;
+bool A4 = TrueFalse(X, Y); Console.Write("Пара А4 - "); Console.WriteLine(A4);
+if(result == A4)
 {
-    Console.WriteLine("Утверждение ¬(12 ⋁ 25) = ¬12 ⋀ ¬25 - НЕВЕРНОЕ! ");
+    Console.Write("Общий результат - ");
+    Console.WriteLine(result);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
