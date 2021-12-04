79154f853d1afae73ccb804199167075e0da798b
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-04 22:55:33 +0300

    add treanjer 19

---
+++ trenajer19/Program.cs
@@ -0,0 +1,41 @@
+﻿int X = 0;
+Console.WriteLine(X);
+int Y = 1;
+Console.WriteLine(Y);
+if(X == 0)
+{
+    Console.WriteLine("Не выполнены условия задачи!!!");
+}
+if(Y == 0)
+{
+    Console.WriteLine("Не выполнены условия задачи!!!");
+}
+
+if(X != 0 && Y != 0)
+{
+    if(X > 0 && Y > 0)
+    {
+        Console.WriteLine("Это первая четверть");
+    }
+}
+if(X != 0 && Y != 0)
+{
+    if(X < 0 && Y > 0)
+    {
+        Console.WriteLine("Это вторая четверть");
+    }
+}
+if(X != 0 && Y != 0)
+{
+    if(X < 0 && Y < 0)
+    {
+        Console.WriteLine("Это третья четверть");
+    }
+}
+if(X != 0 && Y != 0)
+{
+    if(X > 0 && Y < 0)
+    {
+        Console.WriteLine("Это четвертая четверть");
+    }
+}
