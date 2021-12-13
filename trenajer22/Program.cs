f6e681d000f64d009877dc385aa9d8c19cf0ff7a
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-13 23:41:51 +0300

    add trenajer22

---
+++ trenajer22/Program.cs
@@ -0,0 +1,23 @@
+﻿// int xa = 1, ya = 2,
+//     xb = 7, yb = 2;
+
+// int xc = xb - xa;
+// int xd = yb - ya;
+
+// double M = Math.Sqrt(Math.Pow(xc, 2) + Math.Pow(xd, 2));
+// Console.WriteLine(M);
+
+
+
+int xa = 1, ya = 2, za = 3,
+    xb = 7, yb = 2, zb = 4;
+    
+
+int xc = xb - xa;
+int xd = yb - ya;
+int ze = zb - za;
+
+double R = Math.Sqrt(Math.Pow(xc, 2) + Math.Pow(xd, 2) + Math.Pow(ze, 2));
+Console.WriteLine(R);
+
+
