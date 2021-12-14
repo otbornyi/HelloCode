ab9c12d410c7084ac1feb39f1ffe24eff462e6e4
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-14 21:18:51 +0300

    add treanjer 25

---
+++ trenajer25/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = 1;
+int B = new Random().Next(1, 1000);
+int Z = A;
+
+for(int i = 1; i < B; i++)
+{
+    Z = Z + (A + 1);
+    A++;
+}
+Console.WriteLine();
+Console.Write("Факториалом числа ");
+Console.Write(B);
+Console.Write(" является - ");
+Console.WriteLine(Z);
+Console.WriteLine();
