f24760d43e18870f9d51fbf8647c7cd8aad26a2a
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-14 20:42:12 +0300

    add trenajer24

---
+++ trenajer24/Program.cs
@@ -0,0 +1,18 @@
+﻿int A = 1;
+Console.Write("Enter number: ");
+
+int num = int.Parse(Console.ReadLine());
+
+double cube()
+{
+    double C = Math.Pow(A, 3);
+    return C;
+}
+
+for(int i = 1; i < num; i++)
+{
+    Console.Write(A);
+    Console.Write(" в кубе = ");
+    Console.WriteLine(cube());
+    A++;
+}
