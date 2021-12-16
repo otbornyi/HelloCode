ff399e88bc3e6def22122c604629b79e834a29d8
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-16 19:29:25 +0300

    add trenajer 27

---
+++ trenajer27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше число : ");
+string A = Console.ReadLine();
+char[] B = A.ToString().ToCharArray();
+int C = 0;
+
+for(int i = 0; i < B.Length; i++)
+{
+    C++;
+}
+
+Console.Write("Цифр в вашем числе : ");
+Console.WriteLine(C);
+
