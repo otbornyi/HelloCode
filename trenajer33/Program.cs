a27bc810f2a8bf53ebf8b2e9c31b6dd98a58d3b8
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-25 20:28:05 +0300

    add trenajer33

---
+++ trenajer33/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"{A[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void request(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = -A[i];
+    }
+}
+
+int[] A = new int[10];
+FillArray(A);
+PrintArray(A);
+request(A);
+PrintArray(A);
