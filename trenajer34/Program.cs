3a97c6cf70a7ea3026a9d4d1fbd71ac3a327d4c5
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-27 20:43:41 +0300

    add treanjer 34

---
+++ trenajer34/Program.cs
@@ -0,0 +1,39 @@
+﻿void PrintArray(int[] A)
+{
+    for(int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"{A[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] A)
+{
+    for(int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(1, 10);
+    }
+}
+void Finder(int[] A, int B)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        if (A[i] == B)
+        {
+            Console.Write(i);
+            Console.WriteLine();
+            break;
+        }
+    }
+}
+
+
+
+
+int[] A = new int[10];
+int B = 8;
+FillArray(A);
+PrintArray(A);
+Finder(A, B);
+
+
