5f563e3176ddfd8cc2ea3d81730029e4e9c413be
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-28 11:03:36 +0300

    add trenajer 37

---
+++ TRENAJER37/Program.cs
@@ -0,0 +1,34 @@
+﻿void PrintArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"{A[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Find(int[] A)
+{
+
+    int count = 0;
+    for (int i = 0; i < A.Length; i++)
+    {
+        if(A[i] < 100 && A[i] > 9)
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine(count);
+}
+void FillArray(int[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = i + 1;
+    }
+}
+
+int[] A = new int[123];
+FillArray(A);
+PrintArray(A);
+Find(A);
