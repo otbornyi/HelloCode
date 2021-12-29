79a4a78e12ffc69611bfb0f9322ba848d39bf680
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-12-29 16:39:24 +0300

    add trenajer40

---
+++ trenajer40/Program.cs
@@ -0,0 +1,37 @@
+﻿void PrintArray(double[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"{A[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(double[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(1, 10);
+    }
+}
+
+double FindMax(double[] A)
+{
+    double result = 0;    
+    for (int i = 0; i < A.Length; i++)
+    {
+        if(A[i] > result)
+        {
+            result = A[i];
+        }
+    }
+    Console.WriteLine(result);
+    return result;
+}
+
+
+double[] A = new double[6];
+FillArray(A);
+PrintArray(A);
+FindMax(A);
+
