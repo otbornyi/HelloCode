561ed2d70cf960304e9791656afe581e81910c8e
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 18:12:30 +0300

    add trenajer46

---
+++ trenajer46/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            A[i, j] = new Random().Next(10,40);
+        }
+    }
+}
+void PrintArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            Console.Write($"{A[i, j]}   ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int[,] A = new int[3, 8];
+FillArray(A);
+PrintArray(A);
+
+// Заполнение двумерного массива.
