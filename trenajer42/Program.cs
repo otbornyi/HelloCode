fd2b1f08071b43491dcee045549891b17dc0d80c
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 10:34:07 +0300

    add treanjer 42

---
+++ trenajer42/Program.cs
@@ -0,0 +1,14 @@
+﻿int count = 7;
+int A = 0;
+for(int i = 0; i < count; i++)
+{
+    Console.Write("Введите число ");
+    int username = int.Parse(Console.ReadLine());
+    if(username > 0)
+    {
+        A = A + 1;
+    }
+}
+Console.WriteLine($"Количество введеных пользователем положительных цифр - {A}");
+
+
