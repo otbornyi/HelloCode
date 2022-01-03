91f561ef752765745fa9d1034ac10e1d9fcf6e90
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 12:07:35 +0300

    add trenajer 43

---
+++ trenajer43/Program.cs
@@ -0,0 +1,51 @@
+﻿int A = 15487;
+int C = 0;
+int B = A;
+while(B > 1)
+{
+    B = B / 2;
+    C = C + 1;
+}
+C = C + 1;
+
+
+// void PrintArray(int[] Z)
+// {
+//     for (int i = 0; i < Z.Length; i++)
+//     {
+//         Console.Write($"{Z[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+void FillArray(int[] Z)
+{
+    for (int i = 0; i < Z.Length; i++)
+    {
+        if(A % 2 == 0)
+        {
+            Z[i] = 0;
+        }
+        else
+        {
+            Z[i] = 1;
+        }
+        A = A / 2;
+    }
+}
+void returnArray(int[] Z)
+{
+    for (int i = 0; i < Z.Length; i++)
+    {
+        Console.Write($"{Z[C - 1]} ");
+        C = C - 1;
+    }
+
+}
+
+int[] Z = new int[C];
+FillArray(Z);
+// PrintArray(Z);
+returnArray(Z);
+
+// Программа вычисления из десятичной системы в двоичную.
