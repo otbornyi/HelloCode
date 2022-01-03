665c89a0d11b660ef90c5327e6c11aae81b81fad
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 18:32:21 +0300

    add trenajer 47

---
+++ trenajer47/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(double[,] Double)
+{
+    for (int i = 0; i < Double.GetLength(0); i++)
+    {
+        for (int j = 0; j < Double.GetLength(1); j++)
+        {
+            Double[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(double[,] Double)
+{
+    for (int i = 0; i < Double.GetLength(0); i++)
+    {
+        for (int j = 0; j < Double.GetLength(1); j++)
+        {
+            Console.Write($"{Double[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] Double = new double [3, 3];
+FillArray(Double);
+PrintArray(Double);
