93292223fac93c3944ac1d5cb04ef5eeaa671a19
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 10:23:44 +0300

    add trenajer 41

---
+++ trenajer41/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = new Random().Next(1,30);
+int B = new Random().Next(1,30);
+int C = new Random().Next(1,30);
+
+Console.WriteLine($"A = {A}, B = {B}, C = {C}");
+
+if(A < B + C && B < A + C && C < A + B)
+{
+    Console.WriteLine("Да, все окей");
+}
+else
+{
+    Console.WriteLine("Данные числа не являются сторонами треугольника");
+}
