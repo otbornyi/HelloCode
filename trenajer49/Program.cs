3d31df2be5e954b6b51d365815380d8f93171314
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 21:00:15 +0300

    add trenajer 49

---
+++ trenajer49/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            A[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            Console.Write($"{A[i, j]}   ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int[,] A = new int[5, 5];
+FillArray(A);
+PrintArray(A);
