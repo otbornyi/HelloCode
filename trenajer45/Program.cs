f090081154d74db7778ed733ddc1b86315277e14
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-03 15:44:59 +0300

    add trenajer45

---
+++ trenajer45/Program.cs
@@ -0,0 +1,28 @@
+﻿int A = 0;
+Console.Write($"{A} ");
+int B = 1;
+Console.Write($"{B} ");
+void FillArray(int[] Z)
+{
+    for (int i = 0; i < Z.Length; i++)
+    {
+        int C = A + B;
+        Z[i] = C;
+        A = B;
+        B = C;
+    }
+}
+
+void PrintArray(int[] Z)
+{
+    for (int i = 0; i < Z.Length; i++)
+    {
+        Console.Write($"{Z[i]} ");
+    }
+}
+
+int[] Z = new int[20];
+FillArray(Z);
+PrintArray(Z);
+
+// Нахождение фибонначи.
