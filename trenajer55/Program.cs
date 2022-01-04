6471fc468fcf32b7a0bfdc8d7f92d1c8bccba00b
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-04 09:33:37 +0300

    add trenajer55

---
+++ trenajer55/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            A[i, j] = new Random().Next(10,30);
+        }
+    }
+}
+
+void PrintArray(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            Console.Write($"{A[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sum(int[,] A)
+{
+    int B = 0;
+    int count = 0;
+    int P = 0;
+
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            B = B + A[i, j];
+            count++;
+        }
+        B = B / count;
+        P++;
+        Console.WriteLine($"Столбец №{P} средняя арифметическая = {B}");
+        B = 0;
+        count = 0;
+    }
+}
+
+int[,] A = new int[5, 5];
+FillArray(A);
+PrintArray(A);
+Sum(A);
